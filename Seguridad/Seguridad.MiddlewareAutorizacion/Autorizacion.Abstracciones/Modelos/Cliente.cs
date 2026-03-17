a72affaf871e04cefa485aee72d5050bef3976e1
JorgeMadri05 <jorgemadri05@gmail.com>
2026-03-16 20:28:32 -0600

    Prueba 2 - Cambiar el modulo

--- Seguridad/Seguridad.MiddlewareAutorizacion/Autorizacion.Abstracciones/Modelos/Cliente.cs
+++ Seguridad/Seguridad.MiddlewareAutorizacion/Autorizacion.Abstracciones/Modelos/Cliente.cs
@@ -8,29 +8,13 @@ using System.Threading.Tasks;
 
 namespace Abstracciones.Modelos
 {
-    public class ClienteBase
-    {
-        [Required(ErrorMessage = "El campo Nombre es requerido")]
-        public string Nombre { get; set; }
-
-        [Required(ErrorMessage = "El campo Direccion es requerido")]
-        public string Direccion { get; set; }
-
-        [Required(ErrorMessage = "El campo de Pago es requerido")]
-        public string? MetodoPago { get; set; }
-
-
-    }
-
-    public class ClienteRequest : ClienteBase
-    {
-        public Guid idUsuario { get; set; }
-    }
-
-    public class Cliente : ClienteBase
+    public class Cliente
     {
         public Guid idUsuario { get; set; }
         public string Correo { get; set; } = string.Empty;
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string? MetodoPago { get; set; }
 
     }
 }
